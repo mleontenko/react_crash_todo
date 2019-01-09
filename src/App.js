a9b46e75c51f2273afeb72d59921d3094793fd3c
Marin Leontenko <marin.leontenko@gmail.com>
2019-01-09 15:08:08 +0100

    prikaz todo liste

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import Todos from './components/Todos';
 
 class App extends Component {
+  state= {
+    todos: [
+      {
+        id: 1,
+        title: 'Task 1',
+        completed: false
+      }, {
+        id: 2,
+        title: 'Task 2',
+        completed: false
+      }, {
+        id: 3,
+        title: 'Task 3',
+        completed: false
+      }
+    ]
+  }
+
+
   render() {
-    return (
+     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }

a5b20e1e09d79e6c4fd48178f523b50bcfe51352
Marin Leontenko <marin.leontenko@gmail.com>
2019-01-10 14:05:59 +0100

    checkbox za prekrižiti obavljeno

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -13,10 +13,12 @@ export class TodoItem extends Component {
     }
 
     render() {
+        const { id, title } = this.props.todo;
         return (
             <div style={this.getStyle()}>
-                <p>
-                    { this.props.todo.title }
+                <p> 
+                    <input type='checkbox' onChange={this.props.markComplete.bind(this, id)}/> {' '}
+                    { title }
                 </p>
             </div>
         )

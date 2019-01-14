909e57b75d42a9cc3b7a7db6770c1bb39c373a5b
Marin Leontenko <marin.leontenko@gmail.com>
2019-01-14 09:45:54 +0100

    dodan header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
 
 class App extends Component {
@@ -38,7 +39,8 @@ class App extends Component {
   render() {
      return (
       <div className="App">
-        <Todos todos={this.state.todos}  markComplete={this.markComplete} delTodo={this.delTodo}/>
+        <Header />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
   }
